b5417ab29397eeb6a1adb932b068f8fd3a9bf86e
Maşallah <102614198+MasallahErim@users.noreply.github.com>
2023-06-05 20:23:26 +0300

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -54,7 +54,7 @@ X = imp.transform(X)
 
 
 clf = LogisticRegression()
-yhat = cross_val_predict(clf, X, y, cv=10)
+yhat = cross_val_predict(clf, X, y, cv=5)
 
 acc = np.mean(yhat==y)
 tn, fp, fn, tp = confusion_matrix(y, yhat).ravel()
